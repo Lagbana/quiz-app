1951c1f73b56d5e09b3cde2f7583f30552bcaffa
Larry_A <larryagbana@gmail.com>
2020-03-06 23:36:45 -0500

    using a loop through array of question objects to generate option buttons 1 to 4

--- quizzer.js
+++ quizzer.js
@@ -2,47 +2,85 @@ let startButton = document.querySelector('#startbutton')
 let mainContent = document.querySelector('#maincontent')
 
 
-let questions = {
-    question1: "What is the world's biggest island by land mass?",
-    question2: "What is the world's longest river?",
-    question3: "What is the capital of Philippines?",
-    question4: "What is the highest mountain in the world?",
-    question5: "In what continent is Papua New Guinea located?"
-}
 
 
-let options = {
-    option1: ["1. Greenland", "2. Great Britain", "3. Cuba", "4. Madagascar"],
-    option2: ["1. Amazon River", "2. Mekong River", "3. Nile", "4. Yangtze"],
-    option3: ["Kuala Lumpur", "Hanoi", "Bangkok", "Manila"],
-    option4: ["Mount Logan", "Mt. Kilimanjaro", "Mount Everest", "Himalchuli"],
-    option5: ["Africa", "Australia", "South America", "Antarctica"]
-}
+// console.log(quizContent[0]['question'])
 
-let answers = {
-    answer1: "1. Greenland",
-    answer2: "3. Nile",
-    answer3: "Manila",
-    answer4: "Mount Everest",
-    answer5: "Australia"
-}
-
-    function quizBody(whatQuestion) {
+// This function create the html structure for the quiz.
+// The function takes in the index of the question in the quizContent array and is called "questionNumber"
+function quizBody(questionNumber) {
     mainContent.innerHTML = ""
+    // Create questions, options, and answers
+    let question1 = {
+        question: "Question 1: What is the world's biggest island by land mass?",
+        options: ["1. Greenland", "2. Great Britain", "3. Cuba", "4. Madagascar"],
+        answer: "1. Greenland"
+    }
+
+    let question2 = {
+        question: "Question 2: What is the world's longest river?",
+        options: ["1. Amazon River", "2. Mekong River", "3. Nile", "4. Yangtze"],
+        answer: "3. Nile"
+    }
+
+    let question3 = {
+        question: "Question 3: What is the capital of Philippines?",
+        options: ["1. Kuala Lumpur", "2. Hanoi", "3. Bangkok", "4. Manila"],
+        answer: "4. Manila"
+    }
+
+    let question4 = {
+        question: "Question 4: What is the highest mountain in the world?",
+        options: ["1. Mount Logan", "2. Mt. Kilimanjaro", "3. Mount Everest", "4. Himalchuli"],
+        answer: "3. Mount Everest"
+    }
+
+    let question5 = {
+        question: "Question 5: In what continent is Papua New Guinea located?",
+        options: ["1. Africa", "2. Australia", "3. South America", "4. Antarctica"],
+        answer: "2. Australia"
+    }
+
+    let quizContent = [question1, question2, question3, question4, question5]
+
+
+    // Create question H2 element
     const question = document.createElement('h2')
-    question.textContent = questions[whatQuestion]
+    // Get question content
+    question.textContent = quizContent[questionNumber]['question']
     mainContent.appendChild(question)
-    // const options = document.createElement('div')
-    // question1.appendChild(options)
+    // Create spacing
+    const spacing = document.createElement('div')
+    spacing.setAttribute('class', 'p-2')
+    mainContent.appendChild(spacing)
+
+    // Create options Container
+    const optionsContainer = document.createElement('div')
+    spacing.appendChild(optionsContainer)
+    // Create unordered list
+    const optionList = document.createElement('ul')
+    optionList.setAttribute('class', 'list-unstyled')
+    optionsContainer.appendChild(optionList)
+    // Create lists of choices
+    // Use for loop to create option list and option button
+    for (let i = 0; i < quizContent[questionNumber]['options'].length; i++) {
+        const option = document.createElement('li')
+        const optionBtn = document.createElement('button')
+        optionBtn.textContent = quizContent[questionNumber]['options'][i]
+        // optionBtn.setAttribute('class', 'question btn btn-primary mb-3') //STYLING
+        optionBtn.addEventListener("click", function (event) {
+            event.preventDefault();
+            // quizBody(questionNumber + 1)
+        })
+        option.appendChild(optionBtn)
+        optionList.appendChild(option)
+
+    }
+
 }
 
+// Upon click of start button
 startButton.addEventListener("click", function (event) {
     event.preventDefault();
-    quizBody('question1')
-    // mainContent.innerHTML = ""
-    // const question1 = document.createElement('h2')
-    // question.textContent = questions['question1']
-    // mainContent.appendChild(question)
-    // const options = document.createElement('div')
-    // question1.appendChild(options)
+    quizBody(0)
 })
